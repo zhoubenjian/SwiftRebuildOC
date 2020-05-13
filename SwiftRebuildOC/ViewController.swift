73684ed04f1c2a63904048a9adcbc997c1e8ac7d
zhoubenjian <bjzhou@outlook.com>
2020-05-13 16:46:51 +0800

    添加颜色，测试origin/master

--- SwiftRebuildOC/ViewController.swift
+++ SwiftRebuildOC/ViewController.swift
@@ -12,9 +12,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor = .purple
     }
-
-
 }
 
